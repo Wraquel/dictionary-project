101164096c7c11409749a2a1850457f891b03be8
Wraquel <89659478+Wraquel@users.noreply.github.com>
2021-12-08 19:34:15 +0000

    Fixed the function search

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,20 +7,21 @@ export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
 
-  function handlResponse(response) {
+  function handleResponse(response) {
     setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
-
-  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-  axios.get(apiUrl).then(handlResponse);
 
   function handleKeywordChange(event) {
+    event.preventDefault();
     setKeyword(event.target.value);
   }
+
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
