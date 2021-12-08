ce018b639bee99aa8d766e6b219183fb8a1878b8
Wraquel <89659478+Wraquel@users.noreply.github.com>
2021-12-08 12:01:26 +0000

    Add some css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">Hello</header>
+      <button className="btn btn-primary"> Click here</button>
     </div>
   );
 }
